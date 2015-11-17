ff7ed5195b9e67a719cdce329b19c0d9998ba4fb
Carlos Castillo Alarcón <castillo.st@gmail.com>
2015-11-17 16:12:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Node mulicore
+
+This is a test case copy pasted from https://dzone.com/articles/multicore-programming-in-nodejs by [
+Dursun Koç](https://dzone.com/users/874141/dursunKoc.html)
---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+var express = require('express');
+
+module.exports = function(cluster){  
+  var app = express();
+  app.get('/',function(req, res){
+    console.log('Worker %d started!'+new Date(), cluster.worker.id);
+    for(var i = 0; i < 999999999; i++) {
+
+    }
+    res.end('Hello from Worker ' + cluster.worker.id);
+    console.log('Worker %d returned!'+new Date(), cluster.worker.id);
+  });
+  app.listen(8080,function(){
+    console.log('Application started! Worker %d started!, process %d', cluster.worker.id, cluster.worker.process.pid);
+  });
+}
